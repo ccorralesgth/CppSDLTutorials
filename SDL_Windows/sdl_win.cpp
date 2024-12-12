aa9c83b3532a1567cb9713e155171d5255f1cb21
Carlos Corrales <ccdevcreative@outlook.com>
2024-12-12 11:04:36 -0500

    adding readme file, init repo

---
+++ SDL_Windows/sdl_win.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* args[])
+{
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    // Create window
+    SDL_Window* window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == nullptr)
+    {
+        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    // Get window surface
+    SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
+
+    // Fill the surface white
+    SDL_FillRect(screenSurface, nullptr, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+    // Update the surface
+    SDL_UpdateWindowSurface(window);
+
+    // Wait two seconds
+    SDL_Delay(2000);
+
+    // Destroy window
+    SDL_DestroyWindow(window);
+
+    // Quit SDL
+    SDL_Quit();
+
+    return 0;
+}   
